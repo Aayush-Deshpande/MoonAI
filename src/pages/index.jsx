8261471f719a3fefb8ccdcfb7f538ce092e24bb2
Aayush Deshpande <aayushdeshpande532@gmail.com>
2025-11-02 11:07:37 +0530

    Create index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import dynamic from "next/dynamic";
+const VioraRefined = dynamic(() => import("@/components/VioraRefined"), {
+  ssr: false,
+});
+
+export default function Home() {
+  return <VioraRefined />;
+}
+
